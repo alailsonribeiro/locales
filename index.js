76ef968b8e546c9d27da7d7d39a36a346064c571
alailsonribeiro <ribeiro@alailson.com.br>
2020-04-04 13:06:43 -0300

    index.js to show sample and readme with mongodb imports

--- index.js
+++ index.js
@@ -2,10 +2,13 @@
  * @ Author: Aailson Barbosa Ribeiro
  * @ Create Time: 2020-03-30 13:20:41
  * @ Modified by: ribeiro@alailson.com.br
- * @ Modified time: 2020-04-02 15:38:21
+ * @ Modified time: 2020-04-04 13:04:08
  * @ Description: +info: https://alailson.com.br/barbosa-ribeiro-about-sobre
  */
 
-const language = require('./language/detect')
+//const language = require('./language/detect') // in browser
 
-console.log(language())
+//onsole.log(language())
+
+const localset = require('./code/en_US.json')
+console.log(localset)
